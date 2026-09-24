db6a9e06386ace1eac863cb9ad99f2ff92b9c807
TommiRaisanen <t3rato01@students.oamk.fi>
2026-09-24 15:34:44 +0300

    parserikoodia lisätty

--- VK5/timeparser/TimeParser.cpp
+++ VK5/timeparser/TimeParser.cpp
@@ -7,9 +7,21 @@ int time_parse(char *time) {
 
 	// how many seconds, default returns error
 	int seconds = TIME_LEN_ERROR;
-
+	int hours = TIME_LEN_ERROR;
+	int minutes = TIME_LEN_ERROR;
 	// TODO: Check that string is not null
-	
+	if(time == NULL) {
+		return TIME_NULL_ERROR;
+	}
+
+	if (strlen(time) != 6) {
+    	return TIME_LEN_ERROR;
+	}
+	for (int i = 0; i < 6; i++) {
+    	if (time[i] < '0' || time[i] > '9') {
+        	return TIME_VALUE_ERROR;
+    	}
+	}
 	// Parse values from time string
 	// For example: 124033 -> 12hour 40min 33sec
     int values[3];
@@ -22,13 +34,18 @@ int time_parse(char *time) {
 	// values[0] hour
 	// values[1] minute
 	// values[2] second
-
+	hours = values[0];
+	minutes = values[1];
+	seconds = values[2];
 	// TODO: Add boundary check time values: below zero or above limit not allowed
 	// limits are 59 for minutes, 23 for hours, etc
-
+	if(hours < 0 || hours > 23) return TIME_VALUE_ERROR;
+	if(minutes < 0 || minutes > 59) return TIME_VALUE_ERROR;
+	if(seconds < 0 || seconds > 59) return TIME_VALUE_ERROR;
 	// TODO: Calculate return value from the parsed minutes and seconds
 	// Otherwise error will be returned!
 	// seconds = ...
+	seconds = seconds + (minutes * 60);
 
 	return seconds;
 }
--- VK5/timeparser/TimeParser.h
+++ VK5/timeparser/TimeParser.h
@@ -5,6 +5,8 @@
 #define TIME_LEN_ERROR      -1
 #define TIME_ARRAY_ERROR    -2
 #define TIME_VALUE_ERROR    -3
+#define TIME_NULL_ERROR     -4
+#define TIME_BOUNDARY_ERROR -5
 
 using namespace std;
 
